18d88631495c37082020056a816eca274c837bfd
Andre Alves <andresistinfo@gmail.com>
2022-12-15 17:11:40 +0000

    Removing the db file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { HttpModule } from './infra/http/http.module';
-import { PrismaService } from './infra/database/prisma/prisma.service';
 import { Module } from '@nestjs/common';
-import { NotificationsController } from './infra/http/controllers/notification.controller';
 import DatabaseModule from './infra/database/database.module';
 
 @Module({
